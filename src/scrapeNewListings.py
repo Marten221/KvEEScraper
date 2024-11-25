0ed9074cee1db02f868291bbefc6f139e56d85ad
Marten221 <martenoj@ut.ee>
2024-11-25 18:48:13 +0200

    Automated commit Scrape was finished, yielding 87 new listings. Taking 0:02:47.627008

--- src/scrapeNewListings.py
+++ src/scrapeNewListings.py
@@ -6,47 +6,41 @@ from .logger import logger
 from . import listingIdScraper
 from . import listingScraper
 
-scrape_nr = 1
-# Logimine
-# try catch, et kõik kokku ei jookseks ühe postituse pärast #Done
 # Vb proovi andmebaasiga ühendada
 #commitib ainult stc kausta. ja loggeri viimased read eu tule kaasa. Ide crashib kui telost sulgen arvutu.Mingi jama connectioni vahetamisega? peale ugat tsüklit võib driveri sulgeda
-scraperUtils.sleepWithCountdown()
+#scraperUtils.sleepWithCountdown()
 
-while True:
-    driver = scraperUtils.getDriver()
-    # Populate flat-ids2.csv
-    listingIdScraper.scrapeListingIds(driver)
+driver = scraperUtils.getDriver()
+# Populate flat-ids2.csv
+listingIdScraper.scrapeListingIds(driver)
 
-    new_and_old_listings = scraperUtils.readIds("../data/flat-ids2.csv")
-    old_listings = scraperUtils.readIds("../data/flat-ids.csv")
-    only_new_listings = list(set(new_and_old_listings) - set(old_listings))
-    print("Amount of new listings found:", len(only_new_listings), "\n")
+new_and_old_listings = scraperUtils.readIds("../data/flat-ids2.csv")
+old_listings = scraperUtils.readIds("../data/flat-ids.csv")
+only_new_listings = list(set(new_and_old_listings) - set(old_listings))
+print("Amount of new listings found:", len(only_new_listings), "\n")
 
-    scrape_start = datetime.datetime.now()
-    string = f"Started scraping {len(only_new_listings)} listings."
-    print(scrape_start, string)
-    logger.info(string)
+scrape_start = datetime.datetime.now()
+string = f"Started scraping {len(only_new_listings)} listings."
+print(scrape_start, string)
+logger.info(string)
 
-    listingScraper.scrapeListings(driver, only_new_listings)  # Start scraping
+listingScraper.scrapeListings(driver, only_new_listings)  # Start scraping
 
-    scrape_end = datetime.datetime.now()
-    string = f"Finished scraping {len(only_new_listings)} listings."
-    print(scrape_end, string)
-    logger.info(string)
+scrape_end = datetime.datetime.now()
+string = f"Finished scraping {len(only_new_listings)} listings."
+print(scrape_end, string)
+logger.info(string)
 
-    print("Scrape finished in:", scrape_end - scrape_start)
-    scrape_finish_message = f"Scrape nr: {scrape_nr} was finished, yielding {len(only_new_listings)} new listings. Taking {scrape_end - scrape_start}\n"
-    logger.info(scrape_finish_message)
+print("Scrape finished in:", scrape_end - scrape_start)
+scrape_finish_message = f"Scrape was finished, yielding {len(only_new_listings)} new listings. Taking {scrape_end - scrape_start}\n"
+logger.info(scrape_finish_message)
 
-    scraperUtils.appendIds(only_new_listings,
-                           "../data/flat-ids.csv")  # Append freshly scraped listings ids to the scraped listings file
+scraperUtils.appendIds(only_new_listings,
+                       "../data/flat-ids.csv")  # Append freshly scraped listings ids to the scraped listings file
 
-    driver.quit()
-    #Git commit and push
-    scraperUtils.git_commit_and_push(scrape_finish_message)
-    # Sleep
-    scraperUtils.sleepWithCountdown()
-    os.system('clear')
-    scrape_nr += 1
-
+driver.quit()
+#Git commit and push
+scraperUtils.git_commit_and_push(scrape_finish_message)
+# Sleep
+#scraperUtils.sleepWithCountdown()
+os.system('clear')
